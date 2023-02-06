933b9a1ad35b1b47a9c5490cf1a189bd3355ecca
Hidde Elzinga <Hidde.Elzinga@Deltares.nl>
2023-02-06 13:32:37 +0100

    Added some tests for DatasetData

--- tests/data/entities/test_dataset_data.py
+++ tests/data/entities/test_dataset_data.py
@@ -2,6 +2,7 @@
 Tests for DatasetData class
 """
 
+import pytest
 import xarray as _xr
 
 from decoimpact.data.api.i_dataset import IDatasetData
@@ -29,7 +30,7 @@ def test_dataset_data_creation_logic():
 
 def test_dataset_data_get_input_dataset_should_read_file():
     """ When calling get_input_dataset on a dataset should
-    read the specified IDatasetData.path to create a
+    read the specified IDatasetData.path to create a new DataSet
     """
 
     # Arrange
@@ -42,3 +43,40 @@ def test_dataset_data_get_input_dataset_should_read_file():
 
     # Assert
     assert isinstance(dataset, _xr.Dataset)
+
+
+def test_dataset_data_get_input_dataset_should_check_if_path_exists():
+    """ When calling get_input_dataset the provided path
+    needs to be checked if it exists"""
+
+    # Arrange
+    data_dict = {"filename": "non_existing_file.nc", "variable_mapping": {"test": "test_new"}}
+    data = DatasetData(data_dict)
+
+    # Act
+    with pytest.raises(FileExistsError) as exc_info:
+        data.get_input_dataset()
+
+    exception_raised = exc_info.value
+
+    # Assert
+    assert exception_raised.args[0].endswith("Make sure the file location is valid.")
+
+
+def test_dataset_data_get_input_dataset_should_check_if_the_extension_is_correct():
+    """ When calling get_input_dataset the provided path
+    needs to be checked if it exists"""
+
+    # Arrange
+    path = get_test_data_path() + "/NonUgridFile.txt"
+    data_dict = {"filename": path, "variable_mapping": {"test": "test_new"}}
+    data = DatasetData(data_dict)
+
+    # Act
+    with pytest.raises(NotImplementedError) as exc_info:
+        data.get_input_dataset()
+
+    exception_raised = exc_info.value
+
+    # Assert
+    assert exception_raised.args[0].endswith("Currently only UGrid (NetCDF) files are supported.")
