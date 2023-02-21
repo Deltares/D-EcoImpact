2195b17132f38fb66ad55f544b9f0a5da74b70f7
mKlapwijk <maarten.klapwijk@deltares.nl>
2023-02-21 12:01:45 +0100

    DEI-32 Remove double routines

--- decoimpact/data/entities/data_access_layer.py
+++ decoimpact/data/entities/data_access_layer.py
@@ -82,49 +82,6 @@ class DataAccessLayer(IDataAccessLayer):
 
         return dataset
 
-        self._output_dataset = self.copy_dataset(self._input_datasets[0])
-        self._output_dataset = self.remove_variable(
-            self._output_dataset, list_variables
-        )
-
-    def _remove_variable(self, dataset: _xr.Dataset, variable: str) -> _xr.Dataset:
-        """Remove variable from dataset
-
-        Args:
-            dataset (_xr.Dataset): Dataset to remove variable from
-            variable (str/list): Variable(s) to remove
-
-        Raises:
-            ValueError: When variable can not be removed
-
-        Returns:
-            _xr.Dataset: Original dataset
-        """
-        try:
-            dataset = dataset.drop_vars(variable)
-        except ValueError as exc:
-            raise ValueError("ERROR: Cannot remove variable from dataset") from exc
-        return dataset
-
-    def _copy_dataset(self, dataset: _xr.Dataset) -> _xr.Dataset:
-        """Copy dataset to new dataset
-
-        Args:
-            dataset (_xr.Dataset): Dataset to remove variable from
-            variable (str): Variable to remove
-
-        Raises:
-            ValueError: When variable can not be removed
-
-        Returns:
-            _xr.Dataset: Original dataset
-        """
-        try:
-            output_dataset = dataset.copy(deep=False)
-        except ValueError as exc:
-            raise ValueError("ERROR: Cannot copy dataset") from exc
-        return output_dataset
-
     def write_output_file(self, dataset: _xr.Dataset, path: Path) -> None:
         """Write XArray dataset to specified path
 
