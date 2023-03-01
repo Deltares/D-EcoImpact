41f1cd0d829b0fb43856ced3d2fac1b9a233a3a9
mKlapwijk <maarten.klapwijk@deltares.nl>
2023-03-01 15:12:03 +0100

    Fix test to check for all elements in list

--- tests/business/entities/rules/test_step_function_rule/test_step_function_rule.py
+++ tests/business/entities/rules/test_step_function_rule/test_step_function_rule.py
@@ -30,8 +30,8 @@ def test_create_step_function(example_rule):
     # Assert
     assert example_rule._name == "step_function_rule_name"
     assert example_rule.input_variable_names[0] == "input_variable_name"
-    assert example_rule._limits == [0, 1, 2, 5, 10]
-    assert example_rule._responses == [10, 11, 12, 15, 20]
+    assert (example_rule._limits == [0, 1, 2, 5, 10]).all()
+    assert (example_rule._responses == [10, 11, 12, 15, 20]).all()
     assert isinstance(example_rule, StepFunctionRule)
     assert example_rule.validate(logger)
 
