f0bbfbda940dbe2130dc5ddee73c65cbbcffb1bf
IoannaMi <ioannamicha@hotmail.com>
2023-05-30 16:27:50 +0200

    feat[DEI-86]: add resolver for scientific notation reading
    Fix the failing test

--- decoimpact/data/entities/data_access_layer.py
+++ decoimpact/data/entities/data_access_layer.py
@@ -6,6 +6,7 @@ Classes:
 
 """
 
+import re
 from pathlib import Path
 from typing import Any
 
@@ -45,7 +46,9 @@ class DataAccessLayer(IDataAccessLayer):
             raise FileExistsError(msg)
 
         with open(path, "r", encoding="utf-8") as stream:
-            contents: dict[Any, Any] = _yaml.load(stream, Loader=_yaml.FullLoader)
+            contents: dict[Any, Any] = _yaml.load(
+                stream, Loader=self.__create_yaml_loader()
+            )
             model_data_builder = ModelDataBuilder(self._logger)
             return model_data_builder.parse_yaml_data(contents)
 
@@ -121,3 +124,24 @@ class DataAccessLayer(IDataAccessLayer):
             raise OSError(msg) from exc
 
         return None
+
+    def __create_yaml_loader(self):
+
+        loader = _yaml.FullLoader
+        # Add support for scientific notation (example 1e5=100000)
+        loader.add_implicit_resolver(
+            "tag:yaml.org,2002:float",
+            re.compile(
+                """^(?:
+            [-+]?(?:[0-9][0-9_]*)\\.[0-9_]*(?:[eE][-+]?[0-9]+)?
+            |[-+]?(?:[0-9][0-9_]*)(?:[eE][-+]?[0-9]+)
+            |\\.[0-9_]+(?:[eE][-+][0-9]+)?
+            |[-+]?[0-9][0-9_]*(?::[0-5]?[0-9])+\\.[0-9_]*
+            |[-+]?\\.(?:inf|Inf|INF)
+            |\\.(?:nan|NaN|NAN))$""",
+                re.X,
+            ),
+            list("-+0123456789."),
+        )
+
+        return loader
