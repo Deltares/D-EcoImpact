a0c7773f734edfea47c6e0bd03d9df9b7c847dfa
mKlapwijk <maarten.klapwijk@deltares.nl>
2023-08-09 15:14:48 +0200

    Get test_main working

--- tests_acceptance/test_main.py
+++ tests_acceptance/test_main.py
@@ -2,66 +2,26 @@
 Tests for acceptance tests
 """
 
-import pathlib
+
 import runpy
 import sys
+from pathlib import Path
 
 import pytest
 
-input_files = pathlib.Path(__file__, "..", "data").resolve().glob("*.yaml")
-script = pathlib.Path(__file__, "..", "main.py")
+input_files_path = Path(__file__).parent / "input_files"
+input_filenames = [file.name for file in input_files_path.glob("*.yaml")]
+main_script_name = "main.py"
 
 
-@pytest.mark.parametrize("input_file", input_files)
-def test_script_execution(script, input_file):
-    sys.argv = ["", input_file]
-    print("qq", sys.argv)
-    runpy.run_path(script)
+@pytest.mark.parametrize("input_filename", input_filenames)
+def test_process_input(input_filename):
+    input_file_path = input_files_path / input_filename
 
+    # Set up command line arguments for the script
+    sys.argv = ["", str(input_file_path)]
 
-# def pytest_collect_yaml_file(parent, path):
-#     p = pathlib.Path(str(path))
-#     if p.suffix == ".yaml" and p.parent.name == "data":
-#         return Script(path, parent)
+    # Run the script within the same Python process
+    result = runpy.run_path(main_script_name)
 
-
-# class Script(pytest.File):
-#     def collect(self):
-#         yield ScriptItem(self.name, self)
-
-
-# class ScriptItem(pytest.Item):
-#     def runtest(self):
-#         runpy.run_path(self.fspath)
-
-#     def repr_failure(self, excinfo):
-#         excinfo.traceback = excinfo.traceback.cut(path=self.fspath)
-#         return super().repr_failure(excinfo)
-
-
-# def test_running_application():
-#     """Test running application for test file"""
-
-#     # Arrange
-#     logger = Mock(ILogger)
-#     data_layer = Mock(IDataAccessLayer)
-#     model: IModel = Mock(IModel)
-#     model_builder = Mock(IModelBuilder)
-
-#     model.name = "Test model"
-#     model_builder.build_model.return_value = model
-
-#     application = Application(logger, data_layer, model_builder)
-
-#     # Act
-#     application.run("Test.yaml")
-
-#     # Assert
-#     expected_message = 'Model "Test model" has successfully finished running'
-#     logger.log_info.assert_called_with(expected_message)
-
-#     model.validate.assert_called()
-#     model.initialize.assert_called()
-#     model.execute.assert_called()
-#     model.finalize.assert_called()
-#     # assert
+    assert result is not None, f"Script {main_script_name} failed for {input_filename}"
