c13ec13f8e727a6d93d40f2cc3a6af4edc6491bc
mKlapwijk <maarten.klapwijk@deltares.nl>
2023-08-09 15:44:05 +0200

    Convert from runpy to subprocess

--- tests_acceptance/test_main.py
+++ tests_acceptance/test_main.py
@@ -3,8 +3,7 @@ Tests for acceptance tests
 """
 
 
-import runpy
-import sys
+import subprocess
 from pathlib import Path
 
 import pytest
@@ -12,17 +11,25 @@ import pytest
 input_files_path = Path(__file__).parent / "input_yaml_files"
 input_filenames = [file.name for file in input_files_path.glob("*.yaml")]
 main_script_name = "main.py"
+main_script_path = (
+    Path(__file__).parent.parent / "main.py"
+)  # Adjust the path accordingly
 
 
 @pytest.mark.parametrize("input_filename", input_filenames)
 def test_process_input(input_filename):
     input_file_path = input_files_path / input_filename
 
-    # Set up command line arguments for the script
-    sys.argv = ["", input_file_path]
-    print("QQ", sys.argv)
-    print("QQ", sys.argv)
-    # Run the script within the same Python process
-    result = runpy.run_path(main_script_name)
+    # Build the subprocess command
+    command = ["python", str(main_script_path), str(input_file_path)]
 
-    assert result is not None, f"Script {main_script_name} failed for {input_filename}"
+    # Run the script in a separate Python process
+    process = subprocess.Popen(
+        command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
+    )
+    stdout, stderr = process.communicate()
+
+    # Check the exit code
+    assert (
+        process.returncode == 0
+    ), f"Script {main_script_path} failed for {input_filename}\n{stderr}"
