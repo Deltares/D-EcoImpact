6ef89e439a045d13bcf4a91d5ca27d500723f7c7
mKlapwijk <maarten.klapwijk@deltares.nl>
2023-08-09 15:54:12 +0200

    Add reference files location

--- tests_acceptance/test_main.py
+++ tests_acceptance/test_main.py
@@ -11,9 +11,8 @@ import pytest
 input_files_path = Path(__file__).parent / "input_yaml_files"
 input_filenames = [file.name for file in input_files_path.glob("*.yaml")]
 main_script_name = "main.py"
-main_script_path = (
-    Path(__file__).parent.parent / "main.py"
-)  # Adjust the path accordingly
+main_script_path = Path(__file__).parent.parent / main_script_name
+reference_files_path = Path(__file__).parent / "reference_nc_files"
 
 
 @pytest.mark.parametrize("input_filename", input_filenames)
