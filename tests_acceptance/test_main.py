82d18f5ccbea800d8ea4774f9acb4822871cdd0f
mKlapwijk <maarten.klapwijk@deltares.nl>
2023-08-09 15:52:40 +0200

    Revert "update git ignore and add reference files location"
    This reverts commit 5b185ed78e5c78352210e94bb4754451d7e60f84.

--- tests_acceptance/test_main.py
+++ tests_acceptance/test_main.py
@@ -7,18 +7,17 @@ import subprocess
 from pathlib import Path
 
 import pytest
-import xarray as _xr
 
 input_files_path = Path(__file__).parent / "input_yaml_files"
 input_filenames = [file.name for file in input_files_path.glob("*.yaml")]
+main_script_name = "main.py"
 main_script_path = (
     Path(__file__).parent.parent / "main.py"
 )  # Adjust the path accordingly
-reference_files_path = Path(__file__).parent / "reference_nc_files"
 
 
 @pytest.mark.parametrize("input_filename", input_filenames)
-def run_main_and_compare_output_nc_filet(input_filename):
+def test_process_input(input_filename):
     input_file_path = input_files_path / input_filename
 
     # Build the subprocess command
