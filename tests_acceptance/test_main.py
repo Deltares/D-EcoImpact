9c454c542ea1621c13606346821f9ac1e2af8a11
mKlapwijk <maarten.klapwijk@deltares.nl>
2023-08-09 16:14:57 +0200

    rename test case and variables in pytest

--- tests_acceptance/test_main.py
+++ tests_acceptance/test_main.py
@@ -9,16 +9,17 @@ from pathlib import Path
 import pytest
 import xarray as _xr
 
-input_files_path = Path(__file__).parent / "input_yaml_files"
-input_filenames = [file.name for file in input_files_path.glob("*.yaml")]
+input_yaml_files_path = Path(__file__).parent / "input_yaml_files"
+input_yaml_filenames = [file.name for file in input_yaml_files_path.glob("*.yaml")]
 main_script_name = "main.py"
 main_script_path = Path(__file__).parent.parent / main_script_name
+output_nc_files_path = Path(__file__).parent / "output_nc_files"
 reference_files_path = Path(__file__).parent / "reference_nc_files"
 
 
-@pytest.mark.parametrize("input_filename", input_filenames)
+@pytest.mark.parametrize("input_filename", input_yaml_filenames)
 def test_process_input(input_filename):
-    input_file_path = input_files_path / input_filename
+    input_file_path = input_yaml_files_path / input_filename
 
     # Build the subprocess command
     command = ["python", str(main_script_path), str(input_file_path)]
@@ -35,7 +36,9 @@ def test_process_input(input_filename):
     ), f"Script {main_script_path} failed for {input_filename}\n{stderr}"
 
     # Load the generated and reference NetCDF files using xarray
-    generated_nc = _xr.open_dataset("output.nc")  # Update the filename accordingly
+    generated_nc = _xr.open_dataset(
+        output_nc_files_path / input_filename.replace(".yaml", ".nc")
+    )
     reference_nc = _xr.open_dataset(
         reference_files_path / input_filename.replace(".yaml", ".nc")
     )
