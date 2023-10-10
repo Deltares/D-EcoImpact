8af3ccffba3c4840c5f5a433c87ca8da29758481
mKlapwijk <maarten.klapwijk@deltares.nl>
2023-10-10 17:00:09 +0200

    move argument parsing to separate function

--- decoimpact/business/utils/general_utils.py
+++ decoimpact/business/utils/general_utils.py
@@ -1,4 +1,6 @@
+import argparse
 from importlib.metadata import version
+from pathlib import Path
 
 
 def read_version_number():
@@ -9,3 +11,55 @@ def read_version_number():
     """
     version_string = version("decoimpact")
     return version_string
+
+
+def read_command_line_arguments():
+    """Reads the command line arguments given to the tool
+
+    Returns:
+        Path: input yaml path
+    """
+    # Multiline description
+    DESCRIPTION = """
+    # D-EcoImpact
+
+    # A Python based kernel to perform spatial (environmental) impact assessment. Based on knowledge rules applied to model output and/or measurements.
+    # See the README.md for more details
+
+    # Copyright (C) 2022-2023 Stichting Deltares
+    # This program is free software distributed under the
+    # GNU Affero General Public License version 3.0
+    # A copy of the GNU Affero General Public License can be found at
+    # https://github.com/Deltares/D-EcoImpact/blob/main/LICENSE.md
+    """
+
+    # Initialize parser with the multiline description
+    parser = argparse.ArgumentParser(
+        description=DESCRIPTION,
+        formatter_class=argparse.RawTextHelpFormatter,
+    )
+
+    # Adding optional argument
+    parser.add_argument("-i", "--input_file", help="Input yaml file")
+    parser.add_argument(
+        "input_file_positional",
+        nargs="?",
+        help="Input yaml file",
+    )
+    parser.add_argument("-v", "--version", action="store_true", help="Show version")
+
+    # Read arguments from command line
+    args = parser.parse_args()
+
+    if args.input_file:
+        input_path = Path(args.input_file)
+    elif args.input_file_positional:
+        input_path = Path(args.input_file_positional)
+    elif args.version:
+        version = read_version_number()
+        print("D-EcoImpact version:", version)
+        exit()
+    else:
+        parser.print_help()
+        exit()
+    return input_path
--- main.py
+++ main.py
@@ -12,6 +12,7 @@ import sys
 from pathlib import Path
 
 from decoimpact.business.application import Application
+from decoimpact.business.utils.general_utils import read_command_line_arguments
 from decoimpact.business.workflow.model_builder import ModelBuilder
 from decoimpact.crosscutting.i_logger import ILogger
 from decoimpact.crosscutting.logger_factory import LoggerFactory
@@ -36,48 +37,5 @@ def main(path: Path):
 
 
 if __name__ == "__main__":
-
-    # Multiline description
-    description = """
-    # D-EcoImpact
-
-    # A Python based kernel to perform spatial (environmental) impact assessment. Based on knowledge rules applied to model output and/or measurements.
-    # See the README.md for more details
-
-    # Copyright (C) 2022-2023 Stichting Deltares
-    # This program is free software distributed under the
-    # GNU Affero General Public License version 3.0
-    # A copy of the GNU Affero General Public License can be found at
-    # https://github.com/Deltares/D-EcoImpact/blob/main/LICENSE.md
-    """
-
-    # Initialize parser with the multiline description
-    parser = argparse.ArgumentParser(
-        description=description,
-        formatter_class=argparse.RawTextHelpFormatter,
-    )
-
-    # Adding optional argument
-    parser.add_argument("-i", "--input_file", help="Input yaml file")
-    parser.add_argument(
-        "input_file_positional",
-        nargs="?",
-        help="Input yaml file",
-    )
-    parser.add_argument("-v", "--version", action="store_true", help="Show version")
-
-    # Read arguments from command line
-    args = parser.parse_args()
-
-    if args.input_file:
-        input_path = Path(args.input_file)
-    elif args.input_file_positional:
-        input_path = Path(args.input_file_positional)
-    elif args.version:
-        print("VERSION")
-        exit()
-    else:
-        parser.print_help()
-        exit()
-
+    input_path = read_command_line_arguments()
     main(input_path)
