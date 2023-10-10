75e47477b15c6162db9c2ae19ebcc4d3b70d2beb
mKlapwijk <maarten.klapwijk@deltares.nl>
2023-10-10 08:51:27 +0200

    convert constant to UPPER CASE

--- decoimpact/pyinstaller.py
+++ decoimpact/pyinstaller.py
@@ -12,13 +12,13 @@ from pathlib import Path
 import PyInstaller.__main__
 
 HERE = Path(__file__).parent.absolute()
-path_to_main = str(HERE / ".." / "main.py")
+PATH_TO_MAIN = str(HERE / ".." / "main.py")
 
 
 def install():
     PyInstaller.__main__.run(
         [
-            path_to_main,
+            PATH_TO_MAIN,
             "--onefile",
             "--windowed",
             "-ndecoimpact"
