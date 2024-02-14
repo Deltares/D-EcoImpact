db1c64e0f2f1b92e64f026e73e45650f2e89eb8e
Mostafa Farrag <moah.farag@gmail.com>
2024-02-14 16:35:28 +0100

    add github container registry to the image tag

--- build-image.sh
+++ build-image.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-docker build . --file Dockerfile --tag decoimpact:latest
+export IMAGE_ID="decoimpact"
+export VERSION="latest"
+export ARTIFACT_REGISTRY_URL="ghcr.io/deltares"
+export TAG="${ARTIFACT_REGISTRY_URL}/${IMAGE_ID}:${VERSION}"
+
+docker build . --file Dockerfile --tag "${TAG}"
+
+## Pushes image to Artifact Registry
+docker push "${TAG}"
