64cd9dd9bdac38fa8a5e6a3fef5e33670acf4b56
Hidde Elzinga <Hidde.Elzinga@deltares.nl>
2024-05-23 10:18:37 +0200

    Improved parser_rolling_statistics_rule
    Extracted parsing of operation parameters

--- decoimpact/data/parsers/parser_rolling_statistics_rule.py
+++ decoimpact/data/parsers/parser_rolling_statistics_rule.py
@@ -10,7 +10,7 @@ Module for ParserRollingStatisticsRule class
 Classes:
     ParserRollingStatisticsRule
 """
-from typing import Any, Dict
+from typing import Any, Dict, Tuple
 
 from decoimpact.crosscutting.i_logger import ILogger
 from decoimpact.data.api.i_rule_data import IRuleData
@@ -45,42 +45,14 @@ class ParserRollingStatisticsRule(IParserRuleBase):
         time_scale = get_dict_element("time_scale", dictionary)
         period = get_dict_element("period", dictionary)
         description = get_dict_element("description", dictionary, False)
+        output_variable_name = get_dict_element("output_variable", dictionary)
 
-        # if operation contains percentile,
-        # extract percentile value as operation_parameter from operation:
-        if str(operation)[:10] == "PERCENTILE":
-            try:
-                operation_parameter = float(str(operation)[11:-1])
-            except ValueError as exc:
-                message = (
-                    "Operation percentile is missing valid value like 'percentile(10)'"
-                )
-                raise ValueError(message) from exc
-            operation = "PERCENTILE"
-
-        # validate operation
-        match_operation = [o for o in TimeOperationType if o.name == operation]
-        operation_value = next(iter(match_operation), None)
-        operation_parameter = None
-
-        if not operation_value:
-            message = f"Operation is not of a predefined type. Should be in: \
-                      {[o.name for o in TimeOperationType]}. Received: {operation}"
-            raise ValueError(message)
         if not period:
             message = f"Period is not of a predefined type. Should be  \
                       a float or integer value. Received: {period}"
             raise ValueError(message)
-        # test if operation_parameter is within expected limits:
-        if operation_value == TimeOperationType.PERCENTILE:
-            if (
-                operation_parameter is None
-                or operation_parameter < 0
-                or operation_parameter > 100
-            ):
-                message = "Operation percentile should be a number between 0 and 100."
-                raise ValueError(message)
-        output_variable_name = get_dict_element("output_variable", dictionary)
+
+        operation_value, operation_parameter = self._get_operation_values(operation)
 
         rule_data = RollingStatisticsRuleData(
             name,
@@ -95,3 +67,38 @@ class ParserRollingStatisticsRule(IParserRuleBase):
         rule_data.description = description
 
         return rule_data
+
+    def _get_operation_values(
+        self, operation_str: str
+    ) -> Tuple[TimeOperationType, float]:
+        # if operation contains percentile,
+        # extract percentile value as operation_parameter from operation:
+        if str(operation_str)[:10] == "PERCENTILE":
+            try:
+                operation_parameter = float(str(operation_str)[11:-1])
+            except ValueError as exc:
+                message = (
+                    "Operation percentile is missing valid value like 'percentile(10)'"
+                )
+                raise ValueError(message) from exc
+
+            # test if operation_parameter is within expected limits:
+            if (
+                operation_parameter is None
+                or operation_parameter < 0
+                or operation_parameter > 100
+            ):
+                message = "Operation percentile should be a number between 0 and 100."
+                raise ValueError(message)
+            return TimeOperationType.PERCENTILE, operation_parameter
+
+        # validate operation
+        match_operation = [o for o in TimeOperationType if o.name == operation_str]
+        operation_value = next(iter(match_operation), None)
+
+        if not operation_value:
+            message = f"Operation is not of a predefined type. Should be in: \
+                      {[o.name for o in TimeOperationType]}. Received: {operation_str}"
+            raise ValueError(message)
+
+        return operation_value, 0
