d0dc1179a1a0aede0066b4ae57bd41077d7460af
CindyvdVries <cindyvdvries@gmail.com>
2024-05-24 15:19:33 +0200

    update feedback comments

--- decoimpact/data/parsers/parser_classification_rule.py
+++ decoimpact/data/parsers/parser_classification_rule.py
@@ -217,7 +217,7 @@ class ParserClassificationRule(IParserRuleBase):
         if len(smaller) > 1:
             msgs.append(
                 f"{pre_warn}Overlap for variable {name}, multiple criteria with "
-                "operators < or <= are defined"
+                "operators < or <= are defined."
             )
         for i in reversed(smaller[:-1]):
             del sorted_range_criteria[i]
@@ -232,7 +232,7 @@ class ParserClassificationRule(IParserRuleBase):
         if len(larger) > 1:
             msgs.append(
                 f"{pre_warn}Overlap for variable {name}, multiple criteria with "
-                "operators > or >= are defined"
+                "operators > or >= are defined."
             )
         for i in larger[1:]:
             del sorted_range_criteria[i]
@@ -329,5 +329,5 @@ class ParserClassificationRule(IParserRuleBase):
         comp_str = f"range {start}:{end}"
         if (start == end) or (end is None):
             comp_str = f"number {start}"
-        msgs.append(f"{pre_warn}{type_warn} for variable {name} in {comp_str}")
+        msgs.append(f"{pre_warn}{type_warn} for variable {name} in {comp_str}.")
         return msgs
--- tests/data/parsers/test_parser_classification_rule.py
+++ tests/data/parsers/test_parser_classification_rule.py
@@ -62,27 +62,27 @@ def test_parse_dict_to_rule_data_logic():
     [
         (
             [["output", "varA"], [1, "<0"], [2, "<=8"]],
-            """Overlap for variable varA, multiple criteria with operators < or <= are defined\nGap for variable varA in range 8.0:inf""",
+            """Overlap for variable varA, multiple criteria with operators < or <= are defined.\nGap for variable varA in range 8.0:inf.""",
         ),
         (
             [["output", "varB"], [1, ">0"], [2, ">=8"]],
-            """Overlap for variable varB, multiple criteria with operators > or >= are defined\nGap for variable varB in range -inf:0.0""",
+            """Overlap for variable varB, multiple criteria with operators > or >= are defined.\nGap for variable varB in range -inf:0.0.""",
         ),
         (
             [["output", "varC"], [1, "<0"]],
-            """Gap for variable varC in range 0.0:inf""",
+            """Gap for variable varC in range 0.0:inf.""",
         ),
         (
             [["output", "varD"], [1, ">=0"]],
-            """Gap for variable varD in range -inf:0.0""",
+            """Gap for variable varD in range -inf:0.0.""",
         ),
         (
             [["output", "varE"], [1, ">0"], [2, "<10"]],
-            """Overlap for variable varE in range 0.0:10.0""",
+            """Overlap for variable varE in range 0.0:10.0.""",
         ),
         (
             [["output", "varF"], [1, ">0"], [2, "<0"]],
-            """Gap for variable varF in number 0.0""",
+            """Gap for variable varF in number 0.0.""",
         ),
         (
             [["output", "varF2"], [1, ">0"], [2, "<=0"]],
@@ -94,15 +94,15 @@ def test_parse_dict_to_rule_data_logic():
         ),
         (
             [["output", "varG"], [1, ">=0"], [2, "<=0"]],
-            "Overlap for variable varG in number 0.0",
+            "Overlap for variable varG in number 0.0.",
         ),
         (
             [["output", "varH"], [1, "0:10"]],
-            """Gap for variable varH in range -inf:0.0\nGap for variable varH in range 10.0:inf""",
+            """Gap for variable varH in range -inf:0.0.\nGap for variable varH in range 10.0:inf.""",
         ),
         (
             [["output", "varH2"], [1, "0:10"], [1, "10:15"]],
-            """Gap for variable varH2 in range -inf:0.0\nOverlap for variable varH2 in number 10.0\nGap for variable varH2 in range 15.0:inf""",
+            """Gap for variable varH2 in range -inf:0.0.\nOverlap for variable varH2 in number 10.0.\nGap for variable varH2 in range 15.0:inf.""",
         ),
         (
             [["output", "varI"], [1, ">0"], [2, "<0"], [3, 0]],
@@ -110,15 +110,15 @@ def test_parse_dict_to_rule_data_logic():
         ),
         (
             [["output", "varJ"], [1, "<0"], [2, "3:5"], [3, 7]],
-            "Gap for variable varJ in range 0.0:3.0\nGap for variable varJ in range 5.0:7.0\nGap for variable varJ in range 7.0:inf",
+            "Gap for variable varJ in range 0.0:3.0.\nGap for variable varJ in range 5.0:7.0.\nGap for variable varJ in range 7.0:inf.",
         ),
         (
             [["output", "varK"], [1, "0:10"], [2, "3:5"]],
-            "Gap for variable varK in range -inf:0.0\nOverlap for variable varK in range 3.0:5.0\nGap for variable varK in range 10.0:inf",
+            "Gap for variable varK in range -inf:0.0.\nOverlap for variable varK in range 3.0:5.0.\nGap for variable varK in range 10.0:inf.",
         ),
         (
             [["output", "varL"], [1, "0:5"], [2, "10:15"], [3, "15:20"], [4, "7:17"]],
-            "Gap for variable varL in range -inf:0.0\nGap for variable varL in range 5.0:7.0\nOverlap for variable varL in range 10.0:15.0\nOverlap for variable varL in range 15.0:17.0\nGap for variable varL in range 20.0:inf",
+            "Gap for variable varL in range -inf:0.0.\nGap for variable varL in range 5.0:7.0.\nOverlap for variable varL in range 10.0:15.0.\nOverlap for variable varL in range 15.0:17.0.\nGap for variable varL in range 20.0:inf.",
         ),
         (
             [
@@ -129,11 +129,11 @@ def test_parse_dict_to_rule_data_logic():
                 [4, 0],
                 [5, ">=12"],
             ],
-            "Overlap for variable varM in number 0.0\nOverlap for variable varM in number 10.0\nOverlap for variable varM in range 12.0:15.0",
+            "Overlap for variable varM in number 0.0.\nOverlap for variable varM in number 10.0.\nOverlap for variable varM in range 12.0:15.0.",
         ),
         (
             [["output", "varN"], [1, "0:5"], [2, 3], [3, ">=3"]],
-            "Gap for variable varN in range -inf:0.0\nOverlap for variable varN in number 3.0\nOverlap for variable varN in range 3.0:5.0",
+            "Gap for variable varN in range -inf:0.0.\nOverlap for variable varN in number 3.0.\nOverlap for variable varN in range 3.0:5.0.",
         ),
         (
             [["output", "varO"], [2, "-"]],
@@ -141,7 +141,7 @@ def test_parse_dict_to_rule_data_logic():
         ),
         (
             [["output", "varO1"], [1, ">0"], [2, "-"]],
-            "Overlap for variable varO1 in range 0.0:inf",
+            "Overlap for variable varO1 in range 0.0:inf.",
         ),
     ],
 )
@@ -213,7 +213,7 @@ def test_feedback_for_criteria_with_gaps_and_overlap(
             ],
             [
                 call(
-                    """For conditions: (varA: <0, varB: <0). Gap for variable varC in range -inf:0.0\nFor conditions: (varA: <0, varB: >=0). Gap for variable varC in range -inf:0.0\nFor conditions: (varA: <0, varB: >=0). Gap for variable varC in range 10.0:inf\nGap for variable varA in range 0.0:inf"""
+                    """For conditions: (varA: <0, varB: <0). Gap for variable varC in range -inf:0.0.\nFor conditions: (varA: <0, varB: >=0). Gap for variable varC in range -inf:0.0.\nFor conditions: (varA: <0, varB: >=0). Gap for variable varC in range 10.0:inf.\nGap for variable varA in range 0.0:inf."""
                 )
             ],
         ),
@@ -226,25 +226,25 @@ def test_feedback_for_criteria_with_gaps_and_overlap(
             ],
             [
                 call(
-                    """For conditions: (varA: <0, varB: <0). Gap for variable varC in range -inf:0.0\nFor conditions: (varA: <0). Gap for variable varB in range 0.0:inf\nOverlap for variable varA in range -inf:0.0"""
+                    """For conditions: (varA: <0, varB: <0). Gap for variable varC in range -inf:0.0.\nFor conditions: (varA: <0). Gap for variable varB in range 0.0:inf.\nOverlap for variable varA in range -inf:0.0."""
                 )
             ],
         ),
         (
             [
                 ["output", "MIN_water_depth_mNAP", "MAX_flow_velocity", "MAX_chloride"],
-                [1, "<0.10", "-", ">300"],  # to dry
-                [2, "<0.10", "-", "< 400"],  # also to dry
-                [3, ">4.0", ">3.0", "-"],  # to deep
-                [4, ">4.0", "<2.0", "-"],  # also to deep
-                [5, "-", "-", ">400"],  # to salty
-                [6, "-", ">1.5", "-"],  # to fast flowing
-                [7, "-", ">1.5", ">300"],  # alos to fast flowing
+                [1, "<0.10", "-", ">300"],  # too dry
+                [2, "<0.10", "-", "< 400"],  # also too dry
+                [3, ">4.0", ">3.0", "-"],  # too deep
+                [4, ">4.0", "<2.0", "-"],  # also too deep
+                [5, "-", "-", ">400"],  # too salty
+                [6, "-", ">1.5", "-"],  # too fast flowing
+                [7, "-", ">1.5", ">300"],  # also to fast flowing
                 [8, "0.20:4.0", "0.0:1.5", "0:400"],  # perfect for aquatic plants
             ],
             [
                 call(
-                    "For conditions: (MIN_water_depth_mNAP: -, MAX_flow_velocity: -). Gap for variable MAX_chloride in range -inf:400.0\nFor conditions: (MIN_water_depth_mNAP: -, MAX_flow_velocity: >1.5). Overlap for variable MAX_chloride in range 300.0:inf\nFor conditions: (MIN_water_depth_mNAP: 0.20:4.0, MAX_flow_velocity: 0.0:1.5). Gap for variable MAX_chloride in range -inf:0.0\nFor conditions: (MIN_water_depth_mNAP: 0.20:4.0, MAX_flow_velocity: 0.0:1.5). Gap for variable MAX_chloride in range 400.0:inf\nFor conditions: (MIN_water_depth_mNAP: <0.10, MAX_flow_velocity: -). Overlap for variable MAX_chloride in range 300.0:400.0\nFor conditions: (MIN_water_depth_mNAP: -). Overlap for variable MAX_flow_velocity in range 1.5:inf"
+                    "For conditions: (MIN_water_depth_mNAP: -, MAX_flow_velocity: -). Gap for variable MAX_chloride in range -inf:400.0.\nFor conditions: (MIN_water_depth_mNAP: -, MAX_flow_velocity: >1.5). Overlap for variable MAX_chloride in range 300.0:inf.\nFor conditions: (MIN_water_depth_mNAP: 0.20:4.0, MAX_flow_velocity: 0.0:1.5). Gap for variable MAX_chloride in range -inf:0.0.\nFor conditions: (MIN_water_depth_mNAP: 0.20:4.0, MAX_flow_velocity: 0.0:1.5). Gap for variable MAX_chloride in range 400.0:inf.\nFor conditions: (MIN_water_depth_mNAP: <0.10, MAX_flow_velocity: -). Overlap for variable MAX_chloride in range 300.0:400.0.\nFor conditions: (MIN_water_depth_mNAP: -). Overlap for variable MAX_flow_velocity in range 1.5:inf."
                 ),
                 call(
                     "12 warnings found concerning coverage of the parameters. Only first 6 "
