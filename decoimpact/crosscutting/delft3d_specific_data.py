2e4b6020392b483d45e9062cd7491dff9ff98f8c
mKlapwijk <maarten.klapwijk@deltares.nl>
2024-08-02 13:49:44 +0200

    minor review comments

--- decoimpact/crosscutting/delft3d_specific_data.py
+++ decoimpact/crosscutting/delft3d_specific_data.py
@@ -8,15 +8,15 @@
 Configuration file for hardcoded delft3d variable names
 """
 
-INTERFACES_Z_SUFFIX = "_interface_z"
-INTERFACES_SIGMA_SUFFIX = "_interface_sigma"
 BED_LEVEL_SUFFIX = "_flowelem_bl"
+INTERFACES_SIGMA_SUFFIX = "_interface_sigma"
+INTERFACES_Z_SUFFIX = "_interface_z"
 WATER_LEVEL_SUFFIX = "_s1"
 
 
 delft3d_specific_names = [
-    INTERFACES_Z_SUFFIX,
-    INTERFACES_SIGMA_SUFFIX,
     BED_LEVEL_SUFFIX,
+    INTERFACES_SIGMA_SUFFIX,
+    INTERFACES_Z_SUFFIX,
     WATER_LEVEL_SUFFIX
     ]
